ddb14e7db3ad651897b65f211370931d4fcdbe3d
SHAYNE MARC ENZO AHCHOON <shayneah@googlemail.com>
2023-03-11 09:29:12 +0000

    POC: Add Select option

--- src/Components/Calendar/EditorOptions/Flight/Flight.jsx
+++ src/Components/Calendar/EditorOptions/Flight/Flight.jsx
@@ -1,15 +1,41 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Flight.css";
+import { string } from "prop-types";
 
-const Flight = () => {
+export const FLIGHT_TYPES = {
+  DIRECT_DEPARTURE: "Direct Departure",
+  CONNECTING_DEPARTURE: "Connecting Departure",
+  DIRECT_ARRIVAL: "Direct Arrival",
+  CONNECTING_ARRIVAL: "Connecting Arrival",
+};
+
+const Flight = ({type = FLIGHT_TYPES.DIRECT_DEPARTURE}) => {
   return (
-    <>
-      <h3>Where is your departure:</h3>
-      <Link to="/accomodation">Accomodation</Link>
-      <Link to="/flight">Flight</Link>
-    </>
+    <section>
+      <h3>{type} details</h3>
+      <form>
+        <label htmlFor="departure_location">Airport</label>
+        <input id="departure_location" />
+        <label htmlFor="departure_flight">Flight</label>
+        <input id="departure_flight" />
+        <label htmlFor="departure_date">Date</label>
+        <input id="departure_date" placeholder="DD/MM/YYYY" />
+        <label htmlFor="departure_time">Time</label>
+        <input id="departure_location" placeholder="HH:MM" />
+      </form>
+      <hr/>
+      <h3>Add Arrival</h3>
+      <div className="next_options_grid">
+        <Link to="/Flight">Connecting</Link>
+        <Link to="/">Direct</Link>
+      </div>
+    </section>
   );
+};
+
+Flight.propTypes = {
+  type: string,
 };
 
 export default Flight;
--- src/Components/Calendar/EditorOptions/SelectEvent/SelectEvent.jsx
+++ src/Components/Calendar/EditorOptions/SelectEvent/SelectEvent.jsx
@@ -4,12 +4,14 @@ import "./SelectEvent.css";
 
 const SelectEvent = () => {
   return (
-    <>
+    <section>
       <h3>Select your event type:</h3>
-      <Link to="/accomodation">Accomodation</Link>
-      <Link to="/flight">Flight</Link>
-      <Link to="/car">Car</Link>
-    </>
+      <div className="select_event_container">
+        <Link to="/accomodation">Accomodation</Link>
+        <Link to="/flight">Flight</Link>
+        <Link to="/car">Car</Link>
+      </div>
+    </section>
   );
 };
 
